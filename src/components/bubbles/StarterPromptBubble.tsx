21921d899fd4e58caa0aab3968399517d774e3f1
luc4t <basler.lucas@gmail.com>
2024-04-09 13:26:45 +0200

    test rem 3

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -2,26 +2,40 @@ type Props = {
   prompt: string;
   onPromptClick?: () => void;
 };
-export const StarterPromptBubble = (props: Props) => (
-  <>
-    <div
-      data-modal-target="defaultModal"
-      data-modal-toggle="defaultModal"
-      class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
-      onClick={() => props.onPromptClick?.()}
-    >
-      <span
-        class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
-        data-testid="host-bubble"
-        style={{
-          width: 'max-content',
-          'font-size': '0.8rem',
-          'border-radius': '12px',
-          cursor: 'pointer',
-        }}
+
+const getFontSize = () => {
+  const screenWidth = window.innerWidth;
+  if (screenWidth <= 700) {
+    return '12px';
+  } else {
+    return '14px';
+  }
+};
+
+export const StarterPromptBubble = (props: Props) => {
+  const fontSize = getFontSize();
+
+  return (
+    <>
+      <div
+        data-modal-target="defaultModal"
+        data-modal-toggle="defaultModal"
+        class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
+        onClick={() => props.onPromptClick?.()}
       >
-        {props.prompt}
-      </span>
-    </div>
-  </>
-);
+        <span
+          class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+          data-testid="host-bubble"
+          style={{
+            width: 'max-content',
+            'font-size': fontSize,
+            'border-radius': '12px',
+            cursor: 'pointer',
+          }}
+        >
+          {props.prompt}
+        </span>
+      </div>
+    </>
+  );
+};
