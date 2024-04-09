8e4cb1b81553e83dcf23b62f215e4d51daf9be41
luc4t <basler.lucas@gmail.com>
2024-04-09 14:52:17 +0200

    fix voice input

--- src/components/inputs/textInput/components/ShortTextInput.tsx
+++ src/components/inputs/textInput/components/ShortTextInput.tsx
@@ -29,7 +29,7 @@ export const ShortTextInput = (props: ShortTextInputProps) => {
   return (
     <input
       ref={props.ref}
-      class="focus:outline-none bg-transparent px-2 py-3 flex-1 w-full text-input disabled:opacity-50 disabled:cursor-not-allowed disabled:brightness-100"
+      class="focus:outline-none bg-transparent px-4 py-4 flex-1 w-full text-input disabled:opacity-50 disabled:cursor-not-allowed disabled:brightness-100"
       type="text"
       disabled={props.disabled}
       style={{ 'font-size': getFontSize() }}
