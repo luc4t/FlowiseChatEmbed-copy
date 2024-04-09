337972b6fa39f45765de735ee3b172148bc7f727
luc4t <basler.lucas@gmail.com>
2024-04-09 11:03:59 +0200

    test 3

--- dist/components/bubbles/StarterPromptBubble.d.ts
+++ dist/components/bubbles/StarterPromptBubble.d.ts
@@ -2,6 +2,6 @@ type Props = {
     prompt: string;
     onPromptClick?: () => void;
 };
-export declare const StarterPromptBubble: (props: Props) => DocumentFragment;
+export declare const StarterPromptBubble: (props: Props) => string;
 export {};
 //# sourceMappingURL=StarterPromptBubble.d.ts.map
