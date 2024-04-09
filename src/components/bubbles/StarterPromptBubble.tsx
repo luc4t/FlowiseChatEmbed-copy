c561380a429922b3e6f107505db1e5099df66f8f
luc4t <basler.lucas@gmail.com>
2024-04-09 11:03:40 +0200

    test 2

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -4,25 +4,15 @@ type Props = {
 };
 
 export const StarterPromptBubble = (props: Props) => {
-  let isVisible = true;
-
-  const handleClick = () => {
-    isVisible = false;
+  const handleClick = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+    target.style.display = 'none';
     props.onPromptClick?.();
-    updateVisibility();
   };
 
-  const updateVisibility = () => {
-    const hostContainer = document.querySelector('.host-container') as HTMLElement;
-    if (hostContainer) {
-      hostContainer.style.display = isVisible ? 'flex' : 'none';
-    }
-  };
-
-  const template = document.createElement('template');
-  template.innerHTML = `
+  return `
     <div
-      data-modal-target="defaultModal"
+      data-modal-target="defaultModal" 
       data-modal-toggle="defaultModal"
       class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
     >
@@ -30,14 +20,10 @@ export const StarterPromptBubble = (props: Props) => {
         class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
         data-testid="host-bubble"
         style="width: max-content; font-size: 15px; border-radius: 15px; cursor: pointer;"
+        onclick="(${handleClick})(event);"
       >
         ${props.prompt}
       </span>
     </div>
   `;
-
-  const hostContainer = template.content.querySelector('.host-container') as HTMLElement;
-  hostContainer.addEventListener('click', handleClick);
-
-  return template.content;
 };
